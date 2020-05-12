9605a504a50da97bbb4ba4cc4356cc5344059187
Shashang <51124608+shashang29@users.noreply.github.com>
2020-05-12 22:02:10 +0545

    added errorboundary and lazy loading

---
+++ src/services/particlesOptions.js
@@ -0,0 +1,15 @@
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 70,
+            density: {
+                enable: true,
+                value_area: 500
+            }
+        },
+        size: {
+            value: 3
+        }
+    }
+}
+export default particlesOptions;
