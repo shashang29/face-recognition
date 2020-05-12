b64dd8923d91f82aac8f06a84cbf8c237faf66b0
Shashang <51124608+shashang29@users.noreply.github.com>
2020-05-12 21:50:31 +0545

    added errorboundary and lazy loading

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense, lazy } from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 //components
 import ProtectedRoute from './ProtectedRoute';
 import Navigation from '../components/Navigation/Navigation';
-import Signin from '../components/Signin/Signin';
-import Register from '../components/Register/Register';
-import Dashboard from '../containers/Dashboard';
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
 
-
-import { getUserData } from '../actions/userAuth.actions'
-
+import { getUserData } from '../actions/userAuth.actions';
 
 import Particles from 'react-particles-js';
+import particlesOptions from '../services/particlesOptions';
 import './App.css';
 
-
-const particlesOptions = {
-  particles: {
-    number: {
-      value: 70,
-      density: {
-        enable: true,
-        value_area: 500
-      }
-    },
-    size: {
-      value: 3
-    }
-  }
-}
+const Signin = lazy(() => import('../components/Signin/Signin'));
+const Register = lazy(() => import('../components/Register/Register'));
+const Dashboard = lazy(() => import('../containers/Dashboard'));
 
 
 
@@ -43,28 +28,29 @@ const App = ({ getUserDataStart, isSignedIn }) => {
     }
   }, [getUserDataStart])
 
-
   return (
-    <Router >
-      <div className="App">
-        <Particles className='particles'
-          params={particlesOptions}
-        />
-        <Navigation />
-        <Switch>
-          <Route exact path="/" render={() =>
-            isSignedIn ? (<Redirect to='/dashboard' />) : (
-              <Signin />
-            )
-          } />
-          <Route
-            path="/register" component={Register} />
-          <ProtectedRoute path="/dashboard" component={Dashboard}
-          />
-          <Route path="*" component={() => "404 NOT FOUND"} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <Particles className='particles'
+        params={particlesOptions}
+      />
+      <Navigation />
+      <Switch>
+        <ErrorBoundary>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Route exact path="/" render={() =>
+              isSignedIn ? (<Redirect to='/dashboard' />) : (
+                <Signin />
+              )
+            } />
+            <Route
+              path="/register" component={Register} />
+            <ProtectedRoute path="/dashboard" component={Dashboard}
+            />
+            <Route path="*" component={() => "404 NOT FOUND"} />
+          </Suspense>
+        </ErrorBoundary>
+      </Switch>
+    </div>
   );
 }
 
